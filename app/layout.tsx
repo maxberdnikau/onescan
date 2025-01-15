f71d73c37b776427ec7f240205fc639f5451c473
Pavel Mikhel <mashirusesuke@gmail.com>
2025-01-15 14:57:05 +0300

    #1 fixed type error

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,17 @@ import "app/styles/slider.css";
 import "app/styles/tailwind.css";
 import "app/styles/globals.scss";
 
-export async function generateStaticParams() {
-  return [{ lang: 'ru' }, { lang: 'cs' }]
-}
+// export async function generateStaticParams() {
+//   return [{ lang: 'ru' }, { lang: 'cs' }]
+// }
 
 export default function RootLayout({
   children,
-  params,
 }: Readonly<{
   children: React.ReactNode,
-  params: { lang: string }
 }>) {
   return (
-    <html lang={params.lang}>
+    <html>
       <head>
         <meta name="apple-mobile-web-app-title" content="Onescan" />
       </head>
