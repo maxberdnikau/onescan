8128db711b64fa84a657d0b38e8c571026e7e53f
Pavel Mikhel <mashirusesuke@gmail.com>
2025-01-15 14:41:37 +0300

    #1 added mdx support

---
+++ app/reviews/layout.tsx
@@ -0,0 +1,13 @@
+import SiteWrapper from "app/components/site-wrapper";
+
+export default function MdxLayout({ children }: { children: React.ReactNode }) {
+    // Create any shared layout or styles here
+    return <SiteWrapper lang={'cs'}>
+        <main className="flex flex-col static-page">
+            <h1 className="text-center sm:text-left">Отзывы</h1>
+            <article className="flex flex-col">
+                {children}
+            </article>
+        </main>
+    </SiteWrapper>
+  }
