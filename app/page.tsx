d3e3a6feea4a326349412596aeac41b31429999b
Pavel Mikhel <mashirusesuke@gmail.com>
2025-01-15 17:56:29 +0300

    #1 added cs ver of home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import HomeComponent from "@/app/pages/home";
 import { seo_builder } from "app/components/seo";
 
 export const metadata: Metadata = seo_builder({
-  title: 'Профессиональная проверка и индивидуальный подбор автомобилей в Чехии'
+  title: 'Profesionální kontrola a individuální výběr aut'
 });
 
 export default function Home() {
--- app/ru/page.tsx
+++ app/ru/page.tsx
@@ -4,7 +4,7 @@ import HomeComponent from "app/pages/home";
 import { seo_builder } from "app/components/seo";
 
 export const metadata: Metadata = seo_builder({
-    title: 'Профессиональная проверка и индивидуальный подбор автомобилей в Чехии'
+    title: 'Профессиональная проверка и индивидуальный подбор автомобилей'
   });
 
 export default async function HomeRU() {
