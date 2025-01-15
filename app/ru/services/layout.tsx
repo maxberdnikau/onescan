33bfaeac0269976bbd8cdb1aa62827d952835189
Pavel Mikhel <mashirusesuke@gmail.com>
2025-01-15 14:42:59 +0300

    #1 changed lang param

--- app/ru/services/layout.tsx
+++ app/ru/services/layout.tsx
@@ -2,7 +2,7 @@ import SiteWrapper from "app/components/site-wrapper";
 
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
     // Create any shared layout or styles here
-    return <SiteWrapper lang={'cs'}>
+    return <SiteWrapper lang={'ru'}>
         <main className="flex flex-col static-page">
             <h1 className="text-center sm:text-left">Услуги</h1>
             <article className="flex flex-col">
