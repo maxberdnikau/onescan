698b27672ad0fcce6e2d3262e59c943f98e187ec
Pavel Mikhel <mashirusesuke@gmail.com>
2025-01-15 17:24:51 +0300

    #1 changed form on main

--- app/components/main/home/Form/index.tsx
+++ app/components/main/home/Form/index.tsx
@@ -1,121 +1,20 @@
-'use client';
-
-import React from 'react';
-import { useForm } from '@formspree/react';
-
-import SendMessage from './SendMessage';
-
 import styles from './style.module.scss';
 
-const message_close_timeout = 3000;
-
-/**
- * Form on home page
- */
-const Form = ({
+export default function Form ({
     dictionary = {
         form: {
-            input_title: "",
-            submit: "",
-            success_msg: ""
+            mail: "",
         }
     }
-}) => {
-    const [phone, update_phone] = React.useState('');
-    const [show_message, toggle_show_message] = React.useState(false);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [state, handleSubmit] = useForm("xwkyqjpb");
-
-    /**
-     * Add listener when open form message after form submit and remove
-     */
-    React.useEffect(() => {
-        if (show_message) window.addEventListener('keydown', key_listener);
-        else window.removeEventListener('keydown', key_listener);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [show_message])
-
-    /**
-     * Key down listener for `esc`
-     * 
-     * @param {Object} e   keydown data 
-     */
-    const key_listener = (e: KeyboardEvent) => {
-        if (e.key === "Escape") {
-            toggle_show_message(false);
-            window.removeEventListener('keydown', key_listener);
-        }
-        
-        // after cleaner event listener
-        if (show_message === false) window.removeEventListener('keydown', key_listener);
-    }
-
-    /**
-     * Submit handler
-     * 
-     * @param {Element} e   form
-     */
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-
-    //     const form = e.target
-
-    //     fetch('/',
-    //         {
-    //             method: 'POST',
-    //             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    //             body: 
-    //                 encode({
-    //                     'form-name': form.getAttribute('name'),
-    //                     phone
-    //                 }),
-    //         })
-    //     // .then(() => navigate(form.getAttribute('action')))
-    //     .catch((error) => alert(error))
-    // }
-
+}) {
     return(
-        <div>
-            <form className={`${styles.form} mt-[16px] flex flex-col w-full`}
-                // method="post"
-                // name='contact'
-                // action="/"
-                onSubmit={e => {
-                    handleSubmit(e);
-                    update_phone('');
-                    toggle_show_message(true);
-                    setTimeout(() => {
-                        toggle_show_message(false);
-                    }, message_close_timeout);
-                }}
-                // data-netlify="true"
-                // data-netlify-honeypot="bot-field"
-                >
-                {/* <input type="hidden" name="form-name" value="contact" /> */}
-                <input
-                    className={`${styles.form__input} text-center text-2xl placeholder:text-dark-grey`}
-                    type='tel'
-                    name='phone'
-                    placeholder='+420 601 256 408'
-                    pattern="+[0-9]{12}"
-                    title={dictionary.form.input_title}
-                    value={phone}
-                    maxLength={13}
-                    onKeyPress={(e) => {
-                        if (!isFinite(+e.key) && e.key !== '+') e.preventDefault()
-                    }}
-                    onChange={(e) => update_phone(e.target.value)} />
-                <button
-                    type='submit'
-                    className={`${styles.form__submit} text-2xl hover:opacity-75 active:opacity-75`}>
-                    {dictionary.form.submit}
-                </button>
-            </form>
-            <SendMessage
-                show={show_message}
-                success_msg={dictionary.form.success_msg} />
+        <div className='flex flex-col w-[378px] sm:w-full mt-16px'>
+            <a href="mailto:auto@onescan.pro"
+                className={`${styles.link} text-center py-8px px-16px text-2xl hover:opacity-75 active:opacity-75`}
+                target='_blank'
+                rel='noreferrer'>
+                {dictionary.form.mail}
+            </a>
         </div>
     )
-}
-
-export default Form;
+};
