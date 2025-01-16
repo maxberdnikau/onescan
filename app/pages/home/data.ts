11808fa07e194449a240b59985baeb39837dacb1
Pavel Mikhel <mashirusesuke@gmail.com>
2025-01-16 10:11:40 +0300

    #1 changed FAQ block

--- app/pages/home/data.ts
+++ app/pages/home/data.ts
@@ -102,5 +102,35 @@ export const faq_list: faq_item[] = [
     },
     {
         index: "5",
-    }
+    },
+    {
+        index: "6",
+    },
+    {
+        index: "7",
+    },
+    {
+        index: "8",
+    },
+    {
+        index: "9",
+    },
+    {
+        index: "10",
+    },
+    {
+        index: "11",
+    },
+    {
+        index: "12",
+    },
+    {
+        index: "13",
+    },
+    {
+        index: "14",
+    },
+    {
+        index: "15",
+    },
 ];
