53b24d223d86727a3fa8c2743c5d63f83d2e1756
Pavel Mikhel <mashirusesuke@gmail.com>
2025-01-30 09:36:59 +0300

    #1 upd slider image order

--- app/components/main/home/Slider/data.ts
+++ app/components/main/home/Slider/data.ts
@@ -6,48 +6,20 @@ export type slider_item = {
 
 export const slider_data: slider_item[] = [
     {
-        "image": "/assets/slider/1. HONDA CIVIC 1.6 2002 (DE).jpg",
-        "image_alt": "HONDA CIVIC 1.6 2002",
+        "image": "/assets/slider/25. AUDI A4 2.0 TDI 2022 (CZ).jpg",
+        "image_alt": "AUDI A4 2.0 TDI 2022",
     },
     {
-        "image": "/assets/slider/3. Skoda Octavia 4x4 2.0 TDI 2015 (CZ).jpg",
-        "image_alt": "Skoda Octavia 4x4 2.0 TDI 2015",
+        "image": "/assets/slider/19. MB E350 Coupe 2021 (PL).jpg",
+        "image_alt": "MB E350 Coupe 2021",
     },
     {
-        "image": "/assets/slider/4. Skoda Octavia 1.8 TFSI 2013 (DE).jpg",
-        "image_alt": "Skoda Octavia 1.8 TFSI 2013",
-    },
-    {
-        "image": "/assets/slider/5. Toyota Corolla 1.6 2019 (CZ).jpg",
-        "image_alt": "Toyota Corolla 1.6 2019",
-    },
-    {
-        "image": "/assets/slider/6. BMW 530d GT 2014 (DE).jpg",
-        "image_alt": "BMW 530d GT 2014",
-    },
-    {
-        "image": "/assets/slider/8. BMW 740d 2016 (PL).jpg",
-        "image_alt": "BMW 740d 2016",
-    },
-    {
-        "image": "/assets/slider/9. VOLVO V60 2.4D HYBRID 2013 (NL).jpg",
-        "image_alt": "VOLVO V60 2.4D HYBRID 2013",
-    },
-    {
-        "image": "/assets/slider/10. BMW 320d 2019 (DE).jpg",
-        "image_alt": "BMW 320d 2019",
-    },
-    {
-        "image": "/assets/slider/11. VOLVO S60 2.4D (CZ).jpg",
-        "image_alt": "VOLVO S60 2.4D",
+        "image": "/assets/slider/17. MB G63 AMG 2022 (CZ).jpg",
+        "image_alt": "MB G63 AMG 2022",
     },
     {
         "image": "/assets/slider/12. PORSCHE Cayenne S Coupe 4.0 2023 (CZ).jpg",
         "image_alt": "PORSCHE Cayenne S Coupe 4.0 2023",
-    },
-    {
-        "image": "/assets/slider/13. Honda CIVIC 1.8 2007 (CZ).jpg",
-        "image_alt": "Honda CIVIC 1.8 2007",
     },
     {
         "image": "/assets/slider/14. BMW X5 30d 2023 (CZ).jpg",
@@ -62,20 +34,48 @@ export const slider_data: slider_item[] = [
         "image_alt": "BMW X5 30d 2023",
     },
     {
-        "image": "/assets/slider/17. MB G63 AMG 2022 (CZ).jpg",
-        "image_alt": "MB G63 AMG 2022",
+        "image": "/assets/slider/6. BMW 530d GT 2014 (DE).jpg",
+        "image_alt": "BMW 530d GT 2014",
     },
     {
-        "image": "/assets/slider/19. MB E350 Coupe 2021 (PL).jpg",
-        "image_alt": "MB E350 Coupe 2021",
+        "image": "/assets/slider/7. MB ML350 CDI 2010 (DE).jpg",
+        "image_alt": "MB ML350 CDI 2010",
+    },
+    {
+        "image": "/assets/slider/8. BMW 740d 2016 (PL).jpg",
+        "image_alt": "BMW 740d 2016",
+    },
+    {
+        "image": "/assets/slider/9. VOLVO V60 2.4D HYBRID 2013 (NL).jpg",
+        "image_alt": "VOLVO V60 2.4D HYBRID 2013",
+    },
+    {
+        "image": "/assets/slider/10. BMW 320d 2019 (DE).jpg",
+        "image_alt": "BMW 320d 2019",
+    },
+    {
+        "image": "/assets/slider/3. Skoda Octavia 4x4 2.0 TDI 2015 (CZ).jpg",
+        "image_alt": "Skoda Octavia 4x4 2.0 TDI 2015",
+    },
+    {
+        "image": "/assets/slider/4. Skoda Octavia 1.8 TFSI 2013 (DE).jpg",
+        "image_alt": "Skoda Octavia 1.8 TFSI 2013",
+    },
+    {
+        "image": "/assets/slider/5. Toyota Corolla 1.6 2019 (CZ).jpg",
+        "image_alt": "Toyota Corolla 1.6 2019",
+    },
+    {
+        "image": "/assets/slider/11. VOLVO S60 2.4D (CZ).jpg",
+        "image_alt": "VOLVO S60 2.4D",
+    },
+    {
+        "image": "/assets/slider/13. Honda CIVIC 1.8 2007 (CZ).jpg",
+        "image_alt": "Honda CIVIC 1.8 2007",
     },
     {
         "image": "/assets/slider/20. MB E320 CDI 2003 (IT).jpg",
         "image_alt": "MB E320 CDI 2003",
-    },
-    {
-        "image": "/assets/slider/25. AUDI A4 2.0 TDI 2022 (CZ).jpg",
-        "image_alt": "AUDI A4 2.0 TDI 2022",
     },
     {
         "image": "/assets/slider/24. Toyota Corolla 1.6 2010 (CZ).jpg",
@@ -94,15 +94,15 @@ export const slider_data: slider_item[] = [
         "image_alt": "MB E350 CDI 2010",
     },
     {
-        "image": "/assets/slider/7. MB ML350 CDI 2010 (DE).jpg",
-        "image_alt": "MB ML350 CDI 2010",
-    },
-    {
         "image": "/assets/slider/18. MB G63 AMG 2021 (CZ).jpg",
         "image_alt": "MB G63 AMG 2021",
     },
     {
         "image": "/assets/slider/23. Toyota RAV4 2.5 HYBRID 2017 (CZ).jpg",
         "image_alt": "Toyota RAV4 2.5 HYBRID 2017",
+    },
+    {
+        "image": "/assets/slider/1. HONDA CIVIC 1.6 2002 (DE).jpg",
+        "image_alt": "HONDA CIVIC 1.6 2002",
     },
 ];
--- app/components/main/static/reviews/ImagesBlock/index.tsx
+++ app/components/main/static/reviews/ImagesBlock/index.tsx
@@ -1,21 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
 import { nanoid } from "nanoid";
 
-import { slider_data, slider_item } from "app/components/main/home/Slider/data";
+import { image, images_data } from "./data";
 
 export default function ImagesBlock () {
     return(
         <div className="grid grid-cols-2 sm:grid-cols-1 gap-16px mt-48px">
             {
-                slider_data.map((slide: slider_item) => (
+                images_data.map((image_item: image) => (
                     <div key={nanoid()}
                         className="flex flex-col gap-8px">
                         <img
-                            src={slide.image}
-                            alt={slide.image_alt}
+                            src={image_item.image}
+                            alt={image_item.image_alt}
                             loading="lazy"
                             style={{ marginBottom: 0 }} />
-                        <p style={{ marginBottom: 0 }}>{slide.image_alt}</p>
+                        <p style={{ marginBottom: 0 }}>{image_item.image_alt}</p>
                     </div>
                 ))
             }
